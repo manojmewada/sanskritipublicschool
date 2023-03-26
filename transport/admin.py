cb08858a59e8b7541294849b9e7ac23a764fc02c
mewadamanoj948 <mewadamanoj948@gmail.com>
2023-03-26 17:20:34 +0530

    first commit

--- transport/admin.py
+++ transport/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Vehicle, Routes, Driver
-#from import_export import resources
+from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 # Register your models here.,VehicleAdmin
 admin.site.register(Vehicle)
